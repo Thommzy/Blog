c01888acab01b781d2db623763535e72628537c8
Thommzy <tomiwaobeisun@yahoo.com>
2019-10-12 14:21:46 +0100

    admin Login Added

--- public/js/main.js
+++ public/js/main.js
@@ -9,19 +9,50 @@ $(function() {
     success: function(data) {
       console.log('success:', data);
       for (let i = 0; i < data.length; i++) {
-        $postPreview.append(`<a href="specific.html">
-          <h2 class="post-title">
-           ${data[i].post_topic}
-          </h2>
-          <h3 class="post-subtitle">
-            ${data[i].post_content}
-          </h3>
-        </a>
-        <p class="post-meta">
-          Posted by
-          <a href="">Admin</a>
-        </p> <hr />`);
+        console.log(data[i].id);
+        $postPreview.append(`<div class="">
+        <a class="hi" id="${data[i].id}"  href="specific.html">
+        <h2 class="post-title">
+         ${data[i].post_topic}
+        </h2>
+        <h3 class="post-subtitle">
+          ${data[i].post_content}
+        </h3>
+      </a>
+      <p class="post-meta">
+        Posted by
+        <a href="">Admin</a>
+      </p> <hr />
+        </div>
+         `);
       }
+    }
+  });
+
+  $(document).on('click', '.hi', function(e) {
+    var gs = $(this).attr('id');
+    //  alert(`Hello + ${gs}`);
+    localStorage.setItem('postId', gs);
+    console.log(id);
+  });
+
+  var hs = localStorage.getItem('postId');
+
+  var $specMain = $('#specMain');
+
+  $.ajax({
+    type: 'GET',
+    url: `http://localhost:3000/postlists/${hs}`,
+    success: function(data) {
+      console.log('success:', data);
+      $specMain.append(`<div class="container">
+      <h1 class="post_title">${data.post_topic}</h1>
+      <p>By Admin</p>
+      <p>
+        ${data.post_content}
+      </p>
+    </div>
+         `);
     }
   });
 
@@ -180,6 +211,10 @@ if (userCheck) {
 
 // Navbar Button Logic Check Ends
 
+// Specific Post Function Starts
+
+// Specific post function Ends
+
 //Logout Logic Starts
 
 $('#logOut').click(function() {
@@ -188,3 +223,56 @@ $('#logOut').click(function() {
 });
 
 //Logout Logic Ends
+$('.hi').click(function() {
+  alert('logouts');
+});
+
+// Admin Login Ajax Starts
+$('#adminLoginMain').submit(e => {
+  var $loginMain = $('#loginMain');
+  var $loginGreen = $('#loginGreen');
+  e.preventDefault();
+  let adminLoginEmail = $('#adminEmail').val();
+  let adminLoginPassword = $('#adminPassword').val();
+  $.ajax({
+    url: 'http://localhost:3000/admin',
+    method: 'get'
+  }).done(e => {
+    var loginMailCheck = [];
+    var loginPasswordCheck = [];
+    for (let i = 0; i < e.length; i++) {
+      loginMailCheck.push(e[i].signUpEmail);
+      loginPasswordCheck.push(e[i].password);
+      console.log(e[i].signUpEmail);
+    }
+
+    localStorage.setItem('loginEmail!', JSON.stringify(loginMailCheck));
+    localStorage.setItem('loginPassword!', JSON.stringify(loginPasswordCheck));
+
+    let userEmailgottenFromLogin = localStorage.getItem('loginEmail!');
+    let userPasswordgottenFromLogin = localStorage.getItem('loginPassword!');
+
+    if (
+      userEmailgottenFromLogin.indexOf(adminLoginEmail) === -1 ||
+      userPasswordgottenFromLogin.indexOf(adminLoginPassword) === -1
+    ) {
+      $loginMain.append(`<div class="alert alert-danger" role="alert">
+     Login details incorrect
+    </div>`);
+      window.setTimeout(function() {
+        window.location.reload();
+      }, 2000);
+    } else {
+      $loginGreen.append(`<div class="alert alert-success" role="alert">
+        Login Successful
+      </div>`);
+      window.setTimeout(function() {
+        localStorage.setItem('presentEmail', adminLoginEmail);
+        location.href = 'index.html';
+      }, 2000);
+    }
+    console.log(e);
+  });
+});
+
+//Admin Login Ends
